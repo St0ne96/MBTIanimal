0d030e10287dd47b4e60a2447d4e799e2a65fbc9
St0ne96 <bunsuk1637@gmail.com>
2023-06-16 08:28:14 +0900

    kakao share.js 설명 추가

--- js/share.js
+++ js/share.js
@@ -3,13 +3,13 @@ function setShare() {
   const shareUrl = "https://clinquant-phoenix-fe8474.netlify.app";
 
   // 공유할 이미지 URL
-  const shareImage = "img/share.png";
+  const shareImage = "./img/share.png";
 
   // 공유할 제목
-  const shareTitle = "MBTI 유형";
+  const shareTitle = "MBTI 유형 6월 운세";
 
   // 공유할 설명
-  const shareDescription = "MBTI 유형";
+  const shareDescription = "MBTI 유형을 통해 6월에 나의 운세를 확인해보자";
 
   // 카카오링크 생성
   Kakao.Link.sendDefault({
